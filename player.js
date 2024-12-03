e8d7541b1b0308f68916042fc36d4214d224b3e3
LucasDNiguti <niguti005@gmail.com>
2024-12-03 14:54:01 -0300

    Metodos player.js

--- player.js
+++ player.js
@@ -3,5 +3,17 @@ class Player {
     constructor(horizontal, vertical) {
         this.horizontal = horizontal
         this.vertical = vertical
+        this.size = 50
     }
+
+    update(){
+        this.move()
+        this.draw()
+    }
+
+    move(){}
+
+    colisionWall(){}
+
+    draw(){}
 }
