fd2025a45e4f4a355f91dc11bbc1788d5e2d1060
Niguti <niguti005@gmail.com>
2024-12-04 11:02:07 -0300

    corrigindo constructor

--- npc.js
+++ npc.js
@@ -4,7 +4,7 @@ class Npc {
         this.y = initial.y
         this.size = initial.size
         this.step = initial.step
-        this.element = this.createElement
+        this.element = this.createElement();
     }
 
 createElement(){
@@ -12,6 +12,7 @@ createElement(){
     npc.classList.add('npc')
     npc.style.width = `${this.size}px`
     npc.style.height = `${this.size}px`
+    document.body.appendChild(npc);
     return npc
 }
 
