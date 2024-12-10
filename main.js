f88174899a6611327b9e8656775ac762774e511c
Niguti <niguti005@gmail.com>
2024-12-10 10:01:02 -0300

    atualizações
    correções futurasa

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const player = new Player({
-    x: 0,
-    y: 0,
+    x: 50,
+    y: 50,
     size: 50,
     speed: 5,
 });
+
 const npc = new Npc({
     x: 100,
     y: 100,
@@ -11,12 +12,13 @@ const npc = new Npc({
     stepX: 3,
     stepY: 3,
 });
+
 const npc1 = new Npc({
-    x: 100,
-    y: 100,
+    x: 200,
+    y: 200,
     size: 50,
     stepX: 3,
-    stepY: 3
+    stepY: 3,
 });
 
 const game = new Game();
@@ -25,8 +27,4 @@ game.addObject(player);
 game.addObject(npc);
 game.addObject(npc1);
 
-player.draw();
-npc.draw();
-npc1.draw();
-
-requestAnimationFrame(() => game.update());
+game.update();
